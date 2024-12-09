b2b79b92bf3cceaa0fa86beae94dd4c14d558529
Alina Naukhatska <naukhatska.alina@gmail.com>
2024-12-09 22:26:44 +0200

    all

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Profile from "./components/Profile/Profile"
+import user from "./assets/users.json"
+import FriendList from "./components/Frendlist/FriendList";
+import friends from "./assets/friends.json";
+import TransactionHistory from "./components/Transaction/TransactionHistory";
+import transactions from "./assets/transaction.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    return (
+        <div className="App">
+            <Profile
+                name={user.username}
+                tag={user.tag}
+                location={user.location}
+                image={user.avatar}
+                stats={user.stats}
+            />
+            <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
+        </div>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Lalala</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
---
+++ src/components/Frendlist/FriendListItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./FriendList.module.css"
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <div className={styles.friend}>
+      <img className={styles.avatar} src={avatar} alt={`${name}'s avatar`} width="48" />
+      <p className={styles.name}>{name}</p>
+      <p
+        className={`${styles.status} ${
+          isOnline ? styles.online : styles.offline
+        }`}
+      >
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  );
+};
+
+export default FriendListItem;
