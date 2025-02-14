322c9c1ee42b8e577df9b0a1c6c27a24ef8b4bee
Alina Naukhatska <naukhatska.alina@gmail.com>
2025-02-14 22:36:23 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,28 @@ import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
 import SearchBar from './components/SearchBar/SearchBar';
 import ImageModal from './components/ImageModal/ImageModal';
 
-const App = () => {
-  const [photos, setPhotos] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [page, setPage] = useState(1);
-  const [query, setQuery] = useState('');
+interface Photo {
+  id: string;
+  urls: {
+    small: string;
+    regular: string;
+  };
+  alt_description: string;
+}
 
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
+const App: React.FC = () => {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+  const [page, setPage] = useState<number>(1);
+  const [query, setQuery] = useState<string>('');
+
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
   useEffect(() => {
     if (!query) return;
+
     const getPhotodData = async () => {
       try {
         setIsLoading(true);
@@ -28,58 +38,46 @@ const App = () => {
         console.log(results);
       } catch (error) {
         setIsError(true);
-        console.log(error);
+        console.error(error);
       } finally {
         setIsLoading(false);
       }
     };
 
     getPhotodData();
-
   }, [page, query]);
-
 
   const handleChangePage = () => {
     setPage(prev => prev + 1);
-  }
+  };
 
-  const handleChangeQuery = (newQuery) => {
-    if (newQuery === query) {
-      return;
-    }
+  const handleChangeQuery = (newQuery: string) => {
+    if (newQuery === query) return;
     setQuery(newQuery);
     setPhotos([]);
     setPage(1);
-  }
+  };
 
-
-  const handleClickImage = (imageUrl) => {
+  const handleClickImage = (imageUrl: string) => {
     setSelectedImage(imageUrl);
     setIsModalOpen(true);
-  }
+  };
 
   const handleClickImageClose = () => {
     setIsModalOpen(false);
     setSelectedImage(null);
-  }
-
-
+  };
 
   return (
     <div>
-
       <SearchBar onSubmit={handleChangeQuery} />
       {photos.length > 0 && <ImageGallery photos={photos} onImageClick={handleClickImage} />}
       {photos.length > 0 && <LoadMoreBtn onChangePage={handleChangePage} />}
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
-
       <ImageModal isOpen={isModalOpen} onClose={handleClickImageClose} image={selectedImage} />
-
-
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
