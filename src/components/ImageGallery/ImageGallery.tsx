fb2b3db5ba4cca84d4aa95aaddd81e8487387e72
Alina Naukhatska <naukhatska.alina@gmail.com>
2025-02-14 12:13:55 +0200

    Refactor to TypeScript

---
+++ src/components/ImageGallery/ImageGallery.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ImageCard from '../ImageCard/ImageCard';
+import s from './ImageGallery.module.css';
+
+interface ImageGalleryProps {
+  photos: {
+    id: string;
+    urls: {
+      small: string;
+    };
+    alt_description: string;
+  }[];
+  onImageClick: (imageUrl: string) => void;
+}
+
+const ImageGallery: React.FC<ImageGalleryProps> = ({ photos, onImageClick }) => {
+  return (
+    <ul className={s.ul}>
+      {photos.map(item => (
+        <li className={s.li} key={item.id}>
+          <ImageCard item={item} onImageClick={onImageClick} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default ImageGallery;
---
+++ src/components/ImageModal/ImageModal.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Modal from 'react-modal';
+import s from './ImageModal.module.css';
+
+Modal.setAppElement('#root');
+
+interface ImageModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  image: string | null;
+}
+
+const ImageModal: React.FC<ImageModalProps> = ({ isOpen, onClose, image }) => {
+  const handleClose = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      overlayClassName={s.overlay}
+      className={s.modal}
+      onClick={handleClose}
+    >
+      {image && <img src={image} className={s.img} alt="modal" />}
+    </Modal>
+  );
+}
+
+export default ImageModal;
